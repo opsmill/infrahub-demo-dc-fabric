3537b0411720b823c4e5d0e9638693729d592551
John Partee <45423458+morganpartee@users.noreply.github.com>
2023-10-22 18:21:05 +0000

    fix start script

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 pip install ansible
-ansible-galaxy collection install arista.av
+ansible-galaxy collection install arista.avd
 bash -c "$(curl -sL https://get.containerlab.dev)"
 
-curl https://www.arista.com/surl/kc3FX6Rv0v --output ceos.tar.xz
-docker import ceos.tar.xz ceosimage:4.29.0.2F
+git lfs pull
+docker import ceos.tar ceosimage:4.29.0.2F
 
 git clone https://github.com/arista-netdevops-community/avd-cEOS-Lab
 
-cd avd-cEOS-Lab/alpine_host && chmod +x build.sh && ./build.sh
+cd avd-cEOS-Lab/alpine_host
+chmod +x build.sh
+./build.sh
 
 cd ../..
 
