75153c7153851c75e2e9a8f870a9e3da33813148
Benoit Kohler <benoit@opsmill.com>
2023-11-07 15:01:08 +0100

    using codesapce variables

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -7,11 +7,11 @@ poetry install --no-interaction --no-ansi --no-root
 ansible-galaxy collection install arista.avd
 
 # Pull Container Lab images
-docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/ceos-image:4.29.0.2F
-docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/alpine-host:v3.1.1
+docker pull ${HARBOR_HOST}/external/ceos-image:4.29.0.2F
+docker pull ${HARBOR_HOST}/external/alpine-host:v3.1.1
 
 # Pull Infrahub images from our Registry
-docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/opsmill/infrahub-py3.11:0.8.1-init-amd64
+docker pull ${HARBOR_HOST}/opsmill/infrahub-py3.11:0.8.1-init-amd64
 
 # Pull external image directly (linux/amd64)
 docker pull neo4j:5.13-community
