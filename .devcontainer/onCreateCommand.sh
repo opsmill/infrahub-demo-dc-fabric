9b5b05944d6e8396528bed59b2ad781aa6bb3769
Benoit Kohler <benoit@opsmill.com>
2023-11-07 14:21:40 +0100

    update paths and force amd64

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -11,7 +11,7 @@ docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/ceos-image:4.29
 docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/alpine-host:v3.1.1
 
 # Pull Infrahub images from our Registry
-docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/opsmill/infrahub-py3.11:0.8.1-init
+docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/opsmill/infrahub-py3.11:0.8.1-init-amd64
 
 # Pull external image directly (linux/amd64)
 docker pull neo4j:5.13-community
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Docker Compose Infrahub
-docker-compose -f docker-compose.yml -p infrahub up -d
+docker-compose -f ./.devcontainer/docker-compose.yml -p infrahub up -d
 
 # Deploy the lab!
-sudo containerlab deploy -t ../topology/demo.clab.yml --reconfigure
+sudo containerlab deploy -t ./topology/demo.clab.yml --reconfigure
