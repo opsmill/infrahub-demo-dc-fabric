efc97cc3d138d208cdfcf5ebe2c2573df8af23a6
Benoit Kohler <BeArchiTek@users.noreply.github.com>
2023-11-07 12:22:23 +0000

    Update Devcontainer config

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+poetry config virtualenvs.create true
+poetry install --no-interaction --no-ansi --no-root
+
+# Install Arista Collection
 ansible-galaxy collection install arista.avd
 
 # Pull Container Lab images
 docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/ceos-image:4.29.0.2F
 docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/alpine-host:v3.1.1
 
-# Pull Infrahub images
-docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/opsmill/infrahub-py3.11:0.8.1
-docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/neo4j:5.13-community
-docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/redis:7.2
-docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/external/rabbitmq:3.12-management
+# Pull Infrahub images from our Registry
+docker pull 9r2s1098.c1.gra9.container-registry.ovh.net/opsmill/infrahub-py3.11:0.8.1-init
+
+# Pull external image directly (linux/amd64)
+docker pull neo4j:5.13-community
+docker pull redis:7.2
+docker pull rabbitmq:3.12-management
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Docker Compose Infrahub
-docker-compose up -d build docker-compose.yml -p infrahub
+docker-compose -f docker-compose.yml -p infrahub up -d
 
 # Deploy the lab!
-containerlab deploy -t ../topology/demo.clab.yml
+sudo containerlab deploy -t ../topology/demo.clab.yml --reconfigure
