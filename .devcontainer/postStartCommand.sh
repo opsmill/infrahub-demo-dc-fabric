1afe9e9e788233220b0aa9b4fbc894076b7e534b
Benoit Kohler <benoit@opsmill.com>
2023-11-08 10:37:51 +0100

    merge script from infrahub repo + add command in postStart

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -2,4 +2,5 @@
 
 # Load infra-schema and infra-data
 docker compose -f docker-compose.yml -p infrahub run infrahub-git infrahubctl schema load /tmp/models/infrastructure_base.yml
+docker compose -f docker-compose.yml -p infrahub run infrahub-git infrahubctl run /tmp/models/infrastructure_base.py
 docker compose -f docker-compose.yml -p infrahub run infrahub-git infrahubctl run /tmp/models/infrastructure_edge.py
