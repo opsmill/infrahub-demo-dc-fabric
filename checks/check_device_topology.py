f3ff5b80e843d98e8046d2f37899cdef90458780
Elisa Jasinska <elisa@jasinska.de>
2023-11-09 17:46:13 -0700

    more playing with check

--- checks/check_device_topology.py
+++ checks/check_device_topology.py
@@ -4,49 +4,50 @@ from rich import print as rprint
 from infrahub.checks import InfrahubCheck
 
 
-class InfrahubCheckBackboneLinkRedundancy(InfrahubCheck):
+class InfrahubCheckDeviceTopology(InfrahubCheck):
 
-    query = "check_backbone_link_redundancy"
+    query = "check_device_topology"
 
     def validate(self):
+        pass
 
-        site_id_by_name = {}
+        # site_id_by_name = {}
 
-        backbone_links_per_site = defaultdict(lambda: defaultdict(int))
+        # backbone_links_per_site = defaultdict(lambda: defaultdict(int))
 
-        if self.data["data"]["InfraCircuit"]["edges"]:
-            circuits = self.data["data"]["InfraCircuit"]["edges"]
+        # if self.data["data"]["InfraCircuit"]["edges"]:
+        #     circuits = self.data["data"]["InfraCircuit"]["edges"]
 
-            for circuit in circuits:
-                circuit_node = circuit["node"]
-                circuit_status = circuit_node["status"]["node"]["name"]["value"]
+        #     for circuit in circuits:
+        #         circuit_node = circuit["node"]
+        #         circuit_status = circuit_node["status"]["node"]["name"]["value"]
 
-                if circuit_node["endpoints"]["edges"]:
-                    endpoints = circuit_node["endpoints"]["edges"]
+        #         if circuit_node["endpoints"]["edges"]:
+        #             endpoints = circuit_node["endpoints"]["edges"]
 
-                    for endpoint in endpoints:
-                        endpoint_node = endpoint["node"]
-                        site_name = endpoint_node["site"]["node"]["name"]["value"]
+        #             for endpoint in endpoints:
+        #                 endpoint_node = endpoint["node"]
+        #                 site_name = endpoint_node["site"]["node"]["name"]["value"]
 
-                        site_node = endpoint_node["site"]["node"]
-                        site_id_by_name[site_name] = site_node["id"]
-                        backbone_links_per_site[site_name]["total"] += 1
+        #                 site_node = endpoint_node["site"]["node"]
+        #                 site_id_by_name[site_name] = site_node["id"]
+        #                 backbone_links_per_site[site_name]["total"] += 1
 
-                        if endpoint_node["connected_endpoint"]:
-                            connected_endpoint_node = endpoint_node["connected_endpoint"]["node"]
-                            if connected_endpoint_node:
-                                if (connected_endpoint_node["enabled"]["value"] and circuit_status == "active"):
-                                    backbone_links_per_site[site_name]["operational"] += 1
+        #                 if endpoint_node["connected_endpoint"]:
+        #                     connected_endpoint_node = endpoint_node["connected_endpoint"]["node"]
+        #                     if connected_endpoint_node:
+        #                         if (connected_endpoint_node["enabled"]["value"] and circuit_status == "active"):
+        #                             backbone_links_per_site[site_name]["operational"] += 1
 
-            for site_name, site in backbone_links_per_site.items():
-                if site.get("operational", 0) / site["total"] < 0.6:
-                    self.log_error(
-                        message=f"{site_name} has less than 60% of backbone circuit operational ({site.get('operational', 0)}/{site['total']})",
-                        object_id=site_id_by_name[site_name],
-                        object_type="site",
-                    )
+        #     for site_name, site in backbone_links_per_site.items():
+        #         if site.get("operational", 0) / site["total"] < 0.6:
+        #             self.log_error(
+        #                 message=f"{site_name} has less than 60% of backbone circuit operational ({site.get('operational', 0)}/{site['total']})",
+        #                 object_id=site_id_by_name[site_name],
+        #                 object_type="site",
+        #             )
 
-            # rprint(backbone_links_per_site)
+        #     # rprint(backbone_links_per_site)
 
 
-INFRAHUB_CHECKS = [InfrahubCheckBackboneLinkdundancy]
+INFRAHUB_CHECKS = [InfrahubCheckDeviceTopology]
