abb9e0270dfcdc9b0bd87f49973ed135588a9f10
Damien Garros <dgarros@gmail.com>
2023-11-12 10:35:23 -0700

    Centralize Docker image names

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -10,19 +10,10 @@ ansible-galaxy collection install arista.avd
 ansible-galaxy collection install ./.devcontainer/opsmill-infrahub-0.0.1.tar.gz
 
 # Pull Container Lab images
-docker pull "${HARBOR_HOST}"/external/ceos-image:4.29.0.2F
-docker pull "${HARBOR_HOST}"/external/alpine-host:v3.1.1
-
-# Pull Infrahub images from our Registry
-docker pull "${HARBOR_HOST}"/opsmill/infrahub-py3.11:0.8.1-init-amd64
-
-# Pull external image directly (linux/amd64)
-docker pull neo4j:5.13-community
-docker pull redis:7.2
-docker pull rabbitmq:3.12-management
+docker compose -f ./.devcontainer/docker-compose.yml pull
 
 # Docker Compose Infrahub
-docker-compose -f ./.devcontainer/docker-compose.yml -p infrahub up -d
+docker compose -f ./.devcontainer/docker-compose.yml -p infrahub up -d
 
 # Deploy the lab!
 sudo -E containerlab deploy -t ./topology/demo.clab.yml --reconfigure
