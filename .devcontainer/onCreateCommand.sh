aafafb2b041230286bd5dc91465edc395e03ea9f
Damien Garros <dgarros@gmail.com>
2023-11-12 11:54:52 -0700

    Run galaxy command with poetry

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,10 +4,10 @@ poetry config virtualenvs.create true
 poetry install --no-interaction --no-ansi
 
 # Install Arista Collection
-ansible-galaxy install -r ansible-requirements.yml
+poetry run ansible-galaxy install -r ansible-requirements.yml
 
 # Install Opsmill Infrahub Collection (tarball until published)
-ansible-galaxy collection install ./.devcontainer/opsmill-infrahub-0.0.1.tar.gz
+poetry run ansible-galaxy collection install ./.devcontainer/opsmill-infrahub-0.0.1.tar.gz
 
 # Pull Container Lab images
 docker compose -f ./.devcontainer/docker-compose.yml pull
