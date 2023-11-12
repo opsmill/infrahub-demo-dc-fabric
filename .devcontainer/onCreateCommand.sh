233e6bbc3ec0f23a2e182d1278bc0b55f451adb0
Damien Garros <dgarros@gmail.com>
2023-11-12 12:17:31 -0700

    Move docker compose file to the root of the project

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -10,10 +10,10 @@ poetry run ansible-galaxy install -r ansible-requirements.yml
 poetry run ansible-galaxy collection install ./.devcontainer/opsmill-infrahub-0.0.1.tar.gz
 
 # Pull Container Lab images
-docker compose -f ./.devcontainer/docker-compose.yml pull
+docker compose pull
 
 # Docker Compose Infrahub
-docker compose -f ./.devcontainer/docker-compose.yml -p infrahub up -d
+docker compose -p infrahub up -d
 
 # Deploy the lab!
 sudo -E containerlab deploy -t ./topology/demo.clab.yml --reconfigure
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Load infra-schema
-docker compose -f ./.devcontainer/docker-compose.yml -p infrahub run infrahub-git infrahubctl schema load /tmp/models/infrastructure_base.yml
+docker compose -p infrahub run infrahub-git infrahubctl schema load /tmp/models/infrastructure_base.yml
 
 # Load infra-data
-# docker compose -f ./.devcontainer/docker-compose.yml -p infrahub run infrahub-git infrahubctl run /tmp/models/infrastructure_base.py
-# docker compose -f ./.devcontainer/docker-compose.yml -p infrahub run infrahub-git infrahubctl run /tmp/models/infrastructure_edge.py
+# docker compose -p infrahub run infrahub-git infrahubctl run /tmp/models/infrastructure_base.py
+# docker compose -p infrahub run infrahub-git infrahubctl run /tmp/models/infrastructure_edge.py
