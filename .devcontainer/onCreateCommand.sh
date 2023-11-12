3e27a07446e3c74f231c7e3ab18e00aadbde44f0
Damien Garros <dgarros@gmail.com>
2023-11-12 11:12:19 -0700

    Add ansible requirement file

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,7 +4,7 @@ poetry config virtualenvs.create true
 poetry install --no-interaction --no-ansi
 
 # Install Arista Collection
-ansible-galaxy collection install arista.avd
+ansible-galaxy install -r ansible-requirements.yml
 
 # Install Opsmill Infrahub Collection (tarball until published)
 ansible-galaxy collection install ./.devcontainer/opsmill-infrahub-0.0.1.tar.gz
