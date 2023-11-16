b837fc74b561926ec322687c5f44b50a297f24a8
Damien Garros <dgarros@gmail.com>
2023-11-15 21:32:06 -0500

    Remove local collection

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -6,9 +6,6 @@ poetry install --no-interaction --no-ansi
 # Install Arista Collection
 poetry run ansible-galaxy install -r ansible-requirements.yml
 
-# Install Opsmill Infrahub Collection (tarball until published)
-poetry run ansible-galaxy collection install ./.devcontainer/opsmill-infrahub-0.0.3.tar.gz
-
 # Pull Container Lab images
 docker compose pull
 
