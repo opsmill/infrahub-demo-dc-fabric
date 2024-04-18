a3a6ccc2d25e9b25dd3456581eb001c4c0fd38c2
wvandeun <wim@opsmill.com>
2024-04-18 11:09:20 +0200

    load security models and create security nodes using postCreatCommand script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Load infra-schema + infra-topology
-poetry run infrahubctl schema load models
+poetry run infrahubctl schema load models/*.yml
 
 # Wait a bit extra to be sure the schema are properly loaded
 sleep 30
@@ -10,3 +10,4 @@ sleep 30
 poetry run infrahubctl run generators/create_basic.py
 poetry run infrahubctl run generators/create_location.py
 poetry run infrahubctl run generators/create_topology.py
+poetry run infrahubctl run generators/create_security_nodes.py
