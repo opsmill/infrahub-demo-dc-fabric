41d488646284f68f5ec91d2915b9e26d42752660
Marco Martinez <mmmartinez@bluewin.ch>
2024-05-06 22:51:35 +0200

    add sample data

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,3 +8,6 @@ sleep 30
 
 # Load infra-data
 ./scripts/create_demo_data.sh
+
+# Create devices
+./scripts/create_sample_topology.sh
---
+++ scripts/create_sample_topology.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+poetry run infrahubctl run generators/generate_topology.py topology=fra05-pod1
