16523e5e342f64d38ce8a0275f2340c1b6711875
Marco Martinez <mmmartinez@bluewin.ch>
2024-05-09 09:31:34 +0200

    add more sample data

--- scripts/create_sample_topology.sh
+++ scripts/create_sample_topology.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 poetry run infrahubctl run generators/generate_topology.py topology=fra05-pod1
+poetry run infrahubctl run generators/generate_topology.py topology=de1-pod1
+poetry run infrahubctl run generators/generate_topology.py topology=de2-pod1
+
