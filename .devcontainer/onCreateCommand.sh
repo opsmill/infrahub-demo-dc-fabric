a2f916039f92c96b0b3d91260f7a8692c37716d9
wvandeun <wim@opsmill.com>
2024-07-18 14:10:17 +0200

    add invoke tasks to devcontainer

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -6,11 +6,6 @@ poetry install --no-interaction --no-ansi
 # Install Arista Collection
 poetry run ansible-galaxy install -r ansible-requirements.yml
 
-# Pull Container Lab images
-docker compose pull
-
-# Docker Compose Infrahub
-docker compose up -d
-
+poetry run invoke start
 # Deploy the lab!
 # sudo -E containerlab deploy -t ./topology/demo.clab.yml --reconfigure
