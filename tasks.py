673fbc57786e00b80baf90fb15db37449b89ec07
Damien Garros <dgarros@gmail.com>
2024-11-03 16:01:01 +0100

    Add support for local docker compose file

--- tasks.py
+++ tasks.py
@@ -16,35 +16,52 @@ DATA_GENERATORS = [
 # If no version is indicated, we will take the latest
 VERSION = os.getenv("INFRAHUB_VERSION", None)
 COMPOSE_COMMAND = f"curl https://infrahub.opsmill.io/{VERSION if VERSION else ''} | docker compose -f -"
+COMPOSE_COMMAND_LOCAL = "docker compose"
+
+def has_local_docker_file() -> bool:
+    file_path = Path(MAIN_DIRECTORY_PATH, "docker-compose.yml")
+    return file_path.is_file()
+
+
+def get_docker_command() -> str:
+    if has_local_docker_file():
+        return COMPOSE_COMMAND_LOCAL
+    return COMPOSE_COMMAND
+
 
 @task
 def start(context: Context) -> None:
-    context.run(f"{COMPOSE_COMMAND} up -d")
+    with context.cd(MAIN_DIRECTORY_PATH):
+        context.run(f"{get_docker_command()} up -d")
 
 @task
 def load_schema(context: Context, schema: Path=Path("./models/*.yml")) -> None:
-    context.run(f"infrahubctl schema load {schema} --wait")
+    context.run(f"infrahubctl schema load {schema} --wait 30")
 
 @task
 def load_data(context: Context) -> None:
-    for generator in DATA_GENERATORS:
-        context.run(f"infrahubctl run generators/{generator}")
+    with context.cd(MAIN_DIRECTORY_PATH):
+        for generator in DATA_GENERATORS:
+            context.run(f"infrahubctl run generators/{generator}")
 
 @task
 def destroy(context: Context) -> None:
-    context.run(f"{COMPOSE_COMMAND} down -v")
+    with context.cd(MAIN_DIRECTORY_PATH):
+        context.run(f"{get_docker_command()} down -v")
 
 @task
 def stop(context: Context) -> None:
-    context.run(f"{COMPOSE_COMMAND} down")
+    with context.cd(MAIN_DIRECTORY_PATH):
+        context.run(f"{get_docker_command()} down")
 
 @task
 def restart(context: Context, component: str="")-> None:
-    if component:
-        context.run(f"{COMPOSE_COMMAND} restart {component}")
-        return
+    with context.cd(MAIN_DIRECTORY_PATH):
+        if component:
+            context.run(f"{get_docker_command()} restart {component}")
+            return
 
-    context.run(f"{COMPOSE_COMMAND} restart")
+        context.run(f"{get_docker_command()} restart")
 
 
 @task
